bbf0341985214316e52f96fa23a5bc36d29ee1f4
scalySTy <scalySTy@ya.ru>
2024-12-04 11:40:23 +0300

    Remove redundant scripts

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -5,46 +5,38 @@ using UnityEngine;
 public class Counter : MonoBehaviour
 {
     [SerializeField] private float _delay = 0.5f;
-    [SerializeField] private CounterButton _button;
 
     private int _count = 0;
     private Coroutine _coroutine;
     
     public event Action<int> Changed;
     
-    private void OnEnable()
+    private void Update()
     {
-        _button.Clicked += OnButtonClicked;
-    }
-
-    private void OnDisable()
-    {
-        _button.Clicked -= OnButtonClicked;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(_coroutine == null)
+            {
+                _coroutine = StartCoroutine(IncreaseCounter());               
+            }
+            else
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
     }
     
     private IEnumerator IncreaseCounter()
     {
         var wait = new WaitForSeconds(_delay);
 
-        while (true)
+        while (enabled)
         {
             _count++;
             Changed?.Invoke(_count);
             
             yield return wait;
-        }
-    }
-
-    private void OnButtonClicked()
-    {
-        if (_coroutine == null)
-        {
-            _coroutine = StartCoroutine(IncreaseCounter());
-        }
-        else
-        {
-            StopCoroutine(_coroutine);
-            _coroutine = null;
         }
     }
 }
